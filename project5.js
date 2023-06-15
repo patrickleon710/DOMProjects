1f1ade6ef3be060febe33ce5791a11a3e829e592
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-15 07:11:03 +0300

    making the add button work with javascript

---
+++ project5.js
@@ -0,0 +1,35 @@
+// VARIABLES
+
+const inputTask = document.getElementById('input-task')
+const addTask = document.getElementById('add-task')
+const taskContainer = document.getElementById('task-container')
+
+// The add button
+
+addTask.addEventListener('click', function() {
+
+    const task = document.createElement('div')
+    task.classList.add('task')
+
+    const li = document.createElement('li')
+    li.textContent = `${inputTask.value}`
+    task.appendChild(li)
+
+    const checkButton = document.createElement('button')
+    checkButton.innerHTML = `<i class="fa-solid fa-check"></i>` 
+    checkButton.classList.add('check-btn')
+    task.appendChild(checkButton)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+    deleteButton.classList.add('delete-btn')
+    task.appendChild(deleteButton)
+
+    if (inputTask.value === "") {
+        alert("Please enter a task")
+    } else {
+        taskContainer.appendChild(task)
+    }
+
+    inputTask.value = ""
+})
